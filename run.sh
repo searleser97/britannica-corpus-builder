36c594e328e79c2d613f3e5d3acb9504cd407a4d
searleser97 <gabrielsanv97@gmail.com>
2021-10-08 14:41:17 -0500

    updated on 10/08/2021 at 02:41:17 PM

--- run.sh
+++ run.sh
@@ -1,16 +1,13 @@
-bcb gen_raw "2"
-# echo "gen_raw done"
+# bcb gen_raw "2"
 
-bcb gen_corpus
-# echo "gen_corpus done"
+# bcb gen_corpus
 
 bcb split_train_test labeled_sents_preprocessed.txt
-# echo "split done"
 
-./fasttext supervised -input labeled_sents_preprocessed.train -output labeled_sents_preprocessed -autotune-validation labeled_sents_preprocessed.test -autotune-duration 6000
+# ./FastTextBin/fasttext supervised -input labeled_sents_preprocessed.train -output labeled_sents_preprocessed -autotune-validation labeled_sents_preprocessed.test -autotune-duration 6000
 
-# ./fasttext supervised -input labeled_sents_preprocessed.txt -output labeled_sents_preprocessed -lr 0.8 -epoch 30 -wordNgrams 2 -bucket 200000 -dim 300 -thread 8
+./FastTextBin/fasttext supervised -input labeled_sents_preprocessed.txt -output labeled_sents_preprocessed -epoch 37 -minCount 1 -neg 5 -wordNgrams 2 -bucket 4110692 -dim 169 -ws 5 -thread 8 -loss softmax -minn 0 -maxn 0 -lrUpdateRate 100 -t 0.0001
 
-./fasttext test labeled_sents_preprocessed.bin labeled_sents_preprocessed.test 1 0.1
+./FastTextBin/fasttext test labeled_sents_preprocessed.bin labeled_sents_preprocessed.test 1 0.1
 
-./fasttext predict-prob labeled_sents_preprocessed.bin mytests.txt 3 0.1
+bcb predict mytests.txt labeled_sents_preprocessed.bin
